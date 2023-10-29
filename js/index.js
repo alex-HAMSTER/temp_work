1d8dda976f8fbab5800b75c2e8471ec2e7732a92
alex-HAMSTER <alex2003leong@gmail.com>
2023-10-29 22:11:34 +0200

    debaging

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 const menuButton = document.getElementById('menu');
 const menuAnimation = document.getElementById('menu_animation');
 const menuList = document.getElementById('menu_anime');
+const footer = document.querySelector('.footer')
 
-var isTransformed = false;
+let isTransformed = false;
 menuButton.addEventListener('click', function() {
   if (!isTransformed) {
     menuList.style.cssText = 'transform: translateY(-300px)';
     isTransformed = true;
+    footer.style.background = 'none'
   } else {
     menuList.style.cssText = 'transform: translateY(0px)';
     isTransformed = false;
+    footer.style.background = '#fff'
   }
 });
