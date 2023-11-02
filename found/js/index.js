ae11be05854ab1038a00e75dc1cff92d47eba61e
alex-HAMSTER <alex2003leong@gmail.com>
2023-11-02 15:20:06 +0200

    realno zaebal

--- found/js/index.js
+++ found/js/index.js
@@ -22,3 +22,21 @@ lost.addEventListener('click', () => {
     select_2_img.src= 'img/search_earth_red.png'
     select_1_img.src= 'img/search_earth.png'
 });
+
+const menuButton = document.getElementById('menu');
+
+const menuList = document.getElementById('menu_animation');
+const footer = document.querySelector('.footer')
+
+let isTransformed = false;
+menuButton.addEventListener('click', function() {
+  if (!isTransformed) {
+    menuList.style.cssText = 'transform: translateY(-320px)';
+    isTransformed = true;
+    footer.style.background = 'none'
+  } else {
+    menuList.style.cssText = 'transform: translateY(0px)';
+    isTransformed = false;
+    footer.style.background = '#cecece'
+  }
+});
